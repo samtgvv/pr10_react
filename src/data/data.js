91f4a18f8499d301c02e659e0dc219ea3878ee7d
My Name <samat.ibragimov.2000@bk.ru>
2024-11-01 13:15:01 +0300

    111

---
+++ src/data/data.js
@@ -0,0 +1,65 @@
+export const products = [
+    {
+        id: '1',
+        img: '/card1.jpg',
+        name: 'Автопарфюм №1',
+        price: '156',
+        count: 2
+    },
+    {
+        id: '2',
+        img: '/card2.jpg',
+        name: 'Автопарфюм №2',
+        price: '432',
+        count: 0
+    },
+    {
+        id: '3',
+        img: '/card3.jpg',
+        name: 'Автопарфюм №3',
+        price: '102',
+        count: 1
+    },
+    {
+        id: '4',
+        img: '/card4.jpg',
+        name: 'Автопарфюм №4',
+        price: '203',
+        count: 3
+    },
+    {
+        id: '5',
+        img: '/card5.jpg',
+        name: 'Автопарфюм №5',
+        price: '309',
+        count: 6
+    },
+    {
+        id: '6',
+        img: '/card6.jpg',
+        name: 'Автопарфюм №6',
+        price: '178',
+        count:0
+    },
+    {
+        id: '7',
+        img: '/card7.jpg',
+        name: 'Автопарфюм №7',
+        price: '175',
+        count:1
+    },
+    {
+        id: '8',
+        img: '/card8.jpg',
+        name: 'Автопарфюм №8',
+        price: '109',
+        count:2
+    },
+    {
+        id: '9',
+        img: '/card9.jpg',
+        name: 'Автопарфюм №9',
+        price: '305',
+        count:0
+    }
+]
